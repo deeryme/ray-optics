aefb67e918f403882216befd6a92af1c2f9bf6ce
deeryme <12741235-deeryme@users.noreply.gitlab.com>
2025-11-12 14:35:39 +1100

    testing pool starmap

--- handy_files/mp_example.py
+++ handy_files/mp_example.py
@@ -2,16 +2,17 @@
 from multiprocessing import Pool
 import time 
 
-def fn(names_list):
-    for elem in enumerate(names_list):
-        print(f"({elem[0]},{elem[1]})")
+def my_str_fn(idx, val):
+    return f"IDX = {idx}, VAL = {val}"
 
 if __name__ == '__main__':
     names = ['Abe', 'Ben', 'Cal', 'Dan', 'Emi']
     start_t = time.perf_counter()
     names_list = list(enumerate(names))
-    print(names_list)
+    # print(names_list)
     with Pool() as pool:
-        pool.map(fn, names_list)
+        results = pool.starmap(my_str_fn, names_list)
     end_t = time.perf_counter()
     print(f"Elapsed time: {end_t-start_t:.6f} seconds")
+    print(f"Results (len={len(results)}):")
+    print(results)
